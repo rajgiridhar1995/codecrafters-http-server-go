58db30c0494751406cdf48df9d85bdf0bc0c1a17
Roshan <roshan@pop-os.localdomain>
2023-12-29 00:05:32 +0530

    pass 3rd stage

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -20,6 +21,25 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	handleConn(conn)
+}
+
+func handleConn(conn net.Conn) {
+	req := make([]byte, 1000)
+	n, err := conn.Read(req)
+	if err != nil {
+		fmt.Printf("failed to read from connection. Err: %v\n", err)
+		os.Exit(1)
+	}
+	req = req[:n]
+	lines := strings.Split(string(req), "\r\n")
+	tokens := strings.Split(lines[0], " ")
+	path := tokens[1]
+	if path == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+
 	conn.Close()
 }
