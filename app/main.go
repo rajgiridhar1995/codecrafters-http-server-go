d61b0ae60113955659cb5101d8dbf77f903f6269
Roshan <roshan@pop-os.localdomain>
2024-01-05 23:23:18 +0530

    pass 6th stage

--- app/main.go
+++ app/main.go
@@ -16,12 +16,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConn(conn)
 	}
-	handleConn(conn)
 }
 
 func handleConn(conn net.Conn) {
